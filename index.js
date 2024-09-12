92e4a7417867bcaf429908e9dff55da28c4a3435
Joshua Youden <Joshua.Youden@keyin.com>
2024-09-12 09:05:22 -0230

    Caught Up

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const process = require("node:process");
 const arguments = process.argv.slice(2);
 
 if (arguments.includes("--greet")) {
-  console.log("Hello World!");
+  console.log("Hello world!");
+} else if (
+  arguments.includes("--help") ||
+  arguments.includes("-h") ||
+  arguments.includes("help")
+) {
+  console.log(`
+Usage: 
+    --greet     Print 'Hello, World!'
+    --help      Show this help message
+        `);
 } else {
-  console.log("Not Present");
+  console.log(
+    "No valid arguments provided. Use `--help` for usage information."
+  );
 }
